dbd421ecf9e736915caa2b6b0c134543b9725cfa
DmitroOmelianenko <dmitroomelianenko@gmail.com>
2025-07-20 13:59:11 +0300

    Updates

---
+++ blog.a54a6be1.js
@@ -0,0 +1,12 @@
+const t=async()=>{let t=await fetch("https://687cc83d918b6422432f7281.mockapi.io/posts/posts");return await t.json()},e=async t=>{let e=await fetch("https://687cc83d918b6422432f7281.mockapi.io/posts/posts",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(t)});return await e.json()},a=async t=>{await fetch(`https://687cc83d918b6422432f7281.mockapi.io/posts/posts/${t}`,{method:"DELETE"})},i=async(t,e)=>{let a=await fetch(`https://687cc83d918b6422432f7281.mockapi.io/posts/posts/${t}`,{method:"PUT",headers:{"Content-Type":"application/json"},body:JSON.stringify(e)});return await a.json()},s=document.getElementById("postForm"),o=document.getElementById("postList");let n=null;const d=async()=>{o.innerHTML=(await t()).reverse().map(t=>`
+    <li class="post-item" data-id="${t.id}">
+      <h3>${t.title}</h3>
+      <p><b>\u{410}\u{432}\u{442}\u{43E}\u{440}:</b> ${t.author}</p>
+      <p><b>\u{414}\u{430}\u{442}\u{430}:</b> ${t.date}</p>
+      ${t.image?`<img src="${t.image}" alt="">`:""}
+      <p>${t.description}</p>
+      <button class="edit" data-id="${t.id}">\u{270F}\u{FE0F}</button>
+      <button class="del" data-id="${t.id}">\u{274C}</button>
+    </li>
+  `).join(""),o.querySelectorAll(".del").forEach(t=>{t.addEventListener("click",async()=>{await a(t.dataset.id),d()})}),o.querySelectorAll(".edit").forEach(e=>{e.addEventListener("click",async()=>{let a=(await t()).find(t=>t.id==e.dataset.id);a&&(s.title.value=a.title,s.author.value=a.author,s.date.value=a.date,s.image.value=a.image,s.description.value=a.description,n=a.id)})})};s.addEventListener("submit",async t=>{t.preventDefault();let a=Object.fromEntries(new FormData(s));n?await i(n,a):await e(a),n=null,s.reset(),d()}),d();
+//# sourceMappingURL=blog.a54a6be1.js.map
